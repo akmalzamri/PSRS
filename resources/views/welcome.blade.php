4bdf752b6514df6e98c0238721043f9aea9ec3d8
akmalzamri <akmalzamri2@gmail.com>
2020-03-11 16:57:22 +0800

    treatments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -151,7 +151,7 @@
               <div class="media d-block mb-4 text-center site-media site-animate border-0">
                 <img src="images/offer_2.jpg" alt="Free Template by colorlib.com" class="img-fluid">
                 <div class="media-body p-md-5 p-4">
-                  <h5 class="text-primary">$12.50</h5>
+                  <h5 class="text-primary"c>$12.50</h5>
                   <h5 class="mt-0 h4">Fresh Salmon Fish</h5>
                   <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 
@@ -226,7 +226,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 text-center mb-5 site-animate">
-          <h2 class="display-4">Treatments</h2>
+          <h2 class="display-4">Treatmentss</h2>
           <div class="row justify-content-center">
             <div class="col-md-7">
               <p class="lead">Register with us before you make any booking!</p>
